9bf0c5cbafd68881d3b09ff194b3b421576d04aa
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-04-28 13:08:34 -0700

    Got Hello World status for raids

---
+++ Raids.lua
@@ -0,0 +1,13 @@
+api_version = "1.10.1.0"
+--Callbacks
+
+function OnScriptLoad()
+    register_callback(cb['EVENT_COMMAND'], "handleCommand")
+    print("# # # Callbacks registered successfully!")
+end
+
+
+function handleCommand(playerIndex, Command, Env, RconPassword ) --number, string, number, string
+    say(playerIndex, "Welcome to the server!")
+    return false
+end
