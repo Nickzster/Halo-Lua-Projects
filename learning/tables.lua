7c97901a33e08fa9c549c3bcdc08a2d8b9f58c3d
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-05-08 22:27:28 -0700

    seeing what is possible with lua

--- learning/tables.lua
+++ learning/tables.lua
@@ -4,7 +4,9 @@ table = {
     ['hello'] = function(name) print("Hello, " .. name .."!") end,
 }
 
-table['goodbye'] = function(name) print("Goodbye, " .. name .. "!") end
+goodbyeFunction = function(name) print ("Goodbye, " .. name .."!") end
+
+table['goodbye'] = goodbyeFunction
 
 anotherTable = {
     [table] = 10
